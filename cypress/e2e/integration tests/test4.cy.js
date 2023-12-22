3b13adf6865ede86cbfa537a002a3671fb539b5c
tryfkalt <116564459+tryfkalt@users.noreply.github.com>
2023-12-22 13:25:42 +0200

    test to fix

--- cypress/e2e/integration tests/test4.cy.js
+++ cypress/e2e/integration tests/test4.cy.js
@@ -2,29 +2,24 @@
 // Test Case Title: Verify Top Deals Display
 
 describe("Verify Top Deals Display", () => {
-    before(() => {
-      // Adjust the URL to the homepage of the application
-      cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-    });
-  
-    it.only("ensures that the top deals are correctly displayed on the page", () => {
-      // Step 1: Navigate to the 'Top Deals' section of the website.
-      cy.get("a").contains("Top Deals").click();
-  
-      // Step 2: Check the display of the top deals list.
-      cy.get(".products").find(".table-bordered").as("dealsTable");
-      cy.get("@dealsTable").should("be.visible");
-      cy.get("@dealsTable").find("tr").should("have.length.at.least", 1);
-  
-      // Verify that each row contains the expected columns: 'Veg/fruit name', 'Price', and 'Discount price'
-      cy.get("@dealsTable").find("tr").each(($row) => {
-        cy.wrap($row).find("td").eq(0).should("contain.text", "");
-        cy.wrap($row).find("td").eq(1).should("contain.text", "");
-        cy.wrap($row).find("td").eq(2).should("contain.text", "");
-      });
-  
-      // Optionally, check for some known values if necessary
-      cy.get("@dealsTable").contains("td", "Tomato").should("be.visible");
-      cy.get("@dealsTable").contains("td", "Strawberry").should("be.visible");
+  before(() => {
+    // Adjust the URL to the homepage of the application
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+  });
+
+  it.only("ensures that the top deals are correctly displayed on the page", () => {
+    // Step 1: Navigate to the 'Top Deals' section of the website.
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+
+    cy.get(".products").find(".table-bordered").as("dealsTable");
+    cy.get("@dealsTable").should("be.visible");
+    cy.get("@dealsTable").find("tr").should("have.length.at.least", 1);
+
+    // Verify that each row contains the expected columns: 'Veg/fruit name', 'Price', and 'Discount price'
+    cy.get("@dealsTable").find("tr").each(($row) => {
+      cy.wrap($row).find("td").eq(0).should("contain.text", "");
+      cy.wrap($row).find("td").eq(1).should("contain.text", "");
+      cy.wrap($row).find("td").eq(2).should("contain.text", "");
     });
   });
+});
