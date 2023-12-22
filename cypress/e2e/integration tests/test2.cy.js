62462cb9e004ffe111cfc007a4b142c6c7ad2af0
Chrysostomos Koumides <chyrkoum@gmail.com>
2023-12-22 02:32:06 +0200

    check CI

--- cypress/e2e/integration tests/test2.cy.js
+++ cypress/e2e/integration tests/test2.cy.js
@@ -40,7 +40,7 @@ describe("Select Country and Agree to Terms and Conditions", () => {
     homePage.visitHomePage(); 
   });
 
-  it.only("ensures that a user can select their country and agree to the terms and conditions", () => {
+  it("ensures that a user can select their country and agree to the terms and conditions", () => {
     const homePage = new HomePage();
     // Prerequisite: Go to the checkout page.
     homePage.Checkout();
@@ -77,12 +77,12 @@ describe("Attempt to Proceed Without Agreeing to Terms & Conditions", () => {
     homePage.visitHomePage(); 
   });
 
-  it.only("Attempt to Proceed Without Agreeing to Terms & Conditions", () => {
+  it("Attempt to Proceed Without Agreeing to Terms & Conditions", () => {
     const homePage = new HomePage();
     // Prerequisite: Go to the checkout page.
     homePage.Checkout();
     cy.get("button").contains("Place Order").click();
-    
+
     cy.get("select").select("India").should("have.value", "India");
 
     // Step 1: Click 'Proceed' without checking the 'Agree to the Terms & Conditions' checkbox.
@@ -91,5 +91,50 @@ describe("Attempt to Proceed Without Agreeing to Terms & Conditions", () => {
     cy.get(".errorAlert").contains(
       "Please accept Terms & Conditions - Required"
     );
+  });
+});
+
+
+// Test Case ID: TC04
+// Test Case Title: Check if the PROCEED TO CHECKOUT can be clicked without items
+
+// Test Case ID: TC04
+describe("Check if the PROCEED TO CHECKOUT can be clicked without items", () => {
+  before(() => {
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/"); // Adjust the URL to the homepage of the application
+  });
+
+  it("check if the PROCEED TO CHECKOUT can be clicked without items", () => {
+    cy.wait(2000);
+
+    // Step 1: Click on the cart icon to view the cart summary.
+    cy.get(".cart-icon").click();
+
+    // Step 2: Ensure the option to proceed to checkout is available.
+    cy.get(".action-block").contains("PROCEED TO CHECKOUT").click();
+  });
+});
+
+
+
+// Test Case ID: TC07
+// Test Case Title: Attempt to Proceed Without Selecting Country
+
+describe("Attempt to Proceed Without Selecting Country", () => {
+  before(() => {
+    const homePage = new HomePage();
+    homePage.visitHomePage();
+  });
+
+  it.only("Attempt to Proceed Without Selecting Country", () => {
+    const homePage = new HomePage();
+    // Prerequisite: Go to the checkout page.
+    homePage.Checkout();
+    cy.get("button").contains("Place Order").click();
+
+    cy.get(".chkAgree").check().should("be.checked");
+
+    // Step 1: Click the 'Proceed' button without selecting an option from the 'Choose Country' dropdown.
+    cy.get("button").contains("Proceed").click();
   });
 });
